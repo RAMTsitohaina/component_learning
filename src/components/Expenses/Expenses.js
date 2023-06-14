881ff99331a974928724b3055eeae544b3ed4c35
RAMTsitohaina <tsitoramangason@gmail.com>
2023-06-14 21:51:23 +0300

    Rendering Lists & conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 function Expenses(props) {
 
@@ -12,29 +13,17 @@ function Expenses(props) {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+
     return(
         <Card className='expenses'>
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filteredChangeHandler}></ExpensesFilter>
-            <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filteredChangeHandler} />
+
+            <ExpensesChart expenses={filteredExpenses} />
+
+            <ExpensesList items = {filteredExpenses}/>
         </Card>
     )
 }
